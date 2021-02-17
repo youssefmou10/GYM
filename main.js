6c4ade713cd31711d2eb2a4f703bb320177a251c
youssefmou10 <mouddenyoussef98@gmail.com>
2021-02-17 11:26:09 +0100

    created responsive web with other design and implemante map

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-main.js
+function initMap() {
+  // The location of Uluru
+  const casa= { lat: 33.5731104, lng: -7.5898434 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: casa,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: casa,
+    map: map,
+  });
+}
+
+// Smooth scrolling
+$("#navbar a, .btn").on("click", function(event) {
+
+  if(this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate({scrollTop: $(hash).offset().top - 100}, 800);
+  }
+});
+
+
+// navbar Opacity
+
+window.addEventListener("scroll", function() {
+  if(window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
